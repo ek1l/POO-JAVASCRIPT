e54b04cc5935793aa7d5910227dfc119a79e98de
Leonardo Duarte <hintl3rgames2015@gmail.com>
2023-05-03 10:45:10 -0300

    POO - JS

---
+++ Aula 36 - Exercício 8/index.js
@@ -0,0 +1,13 @@
+const caminhao = {
+    nome: "Caminhão",
+    rodas: 4,
+    portas: 2,
+    motor: "Funcionando",
+    BomEstado: true
+}
+
+
+
+const {nome, rodas, portas, motor, BomEstado} = caminhao
+
+console.log(nome)
