eabd23ad28fe78d470a054f45521a12971ffb959
Leonardo Duarte <hintl3rgames2015@gmail.com>
2023-05-03 10:41:34 -0300

    POO - JS

---
+++ Aula 34 - Exercício 6/index.js
@@ -0,0 +1,5 @@
+function instancia(obj1, obj2)  {
+    return obj1 instanceof obj2
+}
+
+
---
+++ Aula 35 - Exercício 7/index.js
@@ -0,0 +1,13 @@
+let n = "numero_de_";
+
+let arvores = {
+  [n + "arvores"]: 10,
+};
+
+let carros = {
+  [n + "carros"]: 20,
+};
+
+console.log(carros);
+
+console.log(arvores);
